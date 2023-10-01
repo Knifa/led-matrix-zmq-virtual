dfac214143e45ed7c84e00ec8b561c45c0cc3b6d
Daniel Callander <knifacat@gmail.com>
2023-10-01 16:53:26 +0100

    Add stuff for LED wall

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@ public:
   enum class Args { endpoint = 1, width, height, scale, bpp };
 
   std::string endpoint = "tcp://*:42024";
-  int width = 128;
-  int height = 128;
-  int scale = 8;
-  int bpp = 24;
+  int width = 320;
+  int height = 192;
+  int scale = 4;
+  int bpp = 32;
 
   static Options from_args(int argc, char *argv[]) {
     Options server_options;
@@ -66,21 +66,26 @@ public:
 int main(int argc, char *argv[]) {
   const auto options = Options::from_args(argc, argv);
 
-  const auto windowWidth = options.width * options.scale;
-  const auto windowHeight = options.height * options.scale;
+  const auto windowWidth = options.height * options.scale;
+  const auto windowHeight = options.width * options.scale;
+
+  const auto textureWidth = options.height;
+  const auto textureHeight = options.width;
 
   SDL_Init(SDL_INIT_VIDEO);
   auto *window =
       SDL_CreateWindow("led-matrix-zmq-virtual", SDL_WINDOWPOS_UNDEFINED,
                        SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, 0);
   auto *renderer = SDL_CreateRenderer(window, -1, 0);
-  auto *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB24,
-                                    SDL_TEXTUREACCESS_STREAMING, options.width,
-                                    options.height);
+  auto *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32,
+                                    SDL_TEXTUREACCESS_STREAMING, textureWidth,
+                                    textureHeight);
 
   zmq::context_t zmqCtx;
   zmq::socket_t zmqSock(zmqCtx, zmq::socket_type::rep);
   zmqSock.bind(options.endpoint);
+
+  uint8_t rawPixels[options.width * options.height * options.bpp / 8];
 
   auto running = true;
   while (running) {
@@ -91,14 +96,29 @@ int main(int argc, char *argv[]) {
       }
     }
 
+    zmqSock.recv(rawPixels,
+                 sizeof(rawPixels));
+    zmqSock.send(nullptr, 0);
+
     void *texturePixels;
     int texturePitch;
     SDL_LockTexture(texture, nullptr, &texturePixels, &texturePitch);
-    zmqSock.recv(texturePixels,
-                 options.width * options.height * options.bpp / 8);
-    zmqSock.send(nullptr, 0);
-    SDL_UnlockTexture(texture);
 
+    // Rotate -90 degrees.
+    for (auto y = 0; y < textureHeight; y++) {
+      for (auto x = 0; x < textureWidth; x++) {
+        const auto srcY = x;
+        const auto srcX = textureHeight - y - 1;
+
+        const auto srcOffset = (srcY * options.width + srcX) * options.bpp / 8;
+        const auto dstOffset = (y * textureWidth + x) * options.bpp / 8;
+
+        std::memcpy(static_cast<uint8_t *>(texturePixels) + dstOffset,
+                    rawPixels + srcOffset, options.bpp / 8);
+      }
+    }
+
+    SDL_UnlockTexture(texture);
     SDL_RenderCopy(renderer, texture, nullptr, nullptr);
     SDL_RenderPresent(renderer);
   }
